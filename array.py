3ddb8e5cfcb76039c1cfc3359c4d5d992cb31a4d
Talison Azzini <143665506+TalisonAzzini@users.noreply.github.com>
2024-09-18 09:11:02 -0300

    Update array.py

--- array.py
+++ array.py
@@ -5,9 +5,9 @@ medias = []
 
 while True:
     print("----- REGISTRO -----")
-    nome = input("Digite o nome do aluno: "))
-    num1 = int(input("Digite o primeiro numero: "))
-    num2 = int(input("Digite o segundo numero: "))
+    nome = input("Digite o nome do aluno: ")
+    num1 = int(input("Digite a primeira nota: "))
+    num2 = int(input("Digite a segunda nota: "))
     media = float((num1+num2)/2)
 
     nomes.append(nome)
